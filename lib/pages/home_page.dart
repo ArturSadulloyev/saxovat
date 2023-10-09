7a9ed4544e604e9a548f3fd103c0ea792b08aba5
Rich <suxrobrich9262@gmail.com>
2023-10-09 16:11:03 +0500

    commit 10.09.2023

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,7 +3,15 @@ import 'package:saxovat/views/font.dart';
 import 'package:swipe_cards/swipe_cards.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  List<AssetImage> images = [
+    AssetImage(
+        'assets/images/integrity-wealth-partners-blog-donor-advised-funds-.jpg'),
+    AssetImage('assets/images/bg-donasi-renew.jpg'),
+    AssetImage('assets/images/shutterstock_396853333-scaled.jpg'),
+    AssetImage('assets/images/wheelchair.jpg'),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -35,27 +43,49 @@ class HomePage extends StatelessWidget {
             height: 10,
           ),
           SizedBox(
-            height: 200,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              primary: false,
-              children: [
-                for (int i = 0; i < 8; i++)
-                  Container(
-                    margin: EdgeInsets.only(left: 10, right: 10),
-                    child: Card(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20)),
-                        elevation: 5,
-                        child: Image(
-                            width: 390,
-                            fit: BoxFit.fill,
-                            image: AssetImage(
-                                'assets/images/integrity-wealth-partners-blog-donor-advised-funds-.jpg'))),
+            height: 240,
+            child: PageView.builder(
+              padEnds: true,
+              pageSnapping: true,
+
+              itemCount: images.length,
+              itemBuilder: (context, index) => Container(
+                margin: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+                height: 100,
+                width: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Stack(
+                    alignment: Alignment.bottomCenter,
+                    children: [
+                      Image(
+                        image: images[index],
+                        height: double.maxFinite,
+                        width: double.maxFinite,
+                        fit: BoxFit.cover,
+                      ),
+                      Positioned(
+                        bottom: 20,
+                        child: Expanded(
+                          child: Text(
+                            "Bu yerda qandaydir manoli so'zlar bo'lishi mumkin edi Artur !",
+                            style: font(
+                              size: 18,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
                   ),
-              ],
+                ),
+              ),
             ),
           ),
+          Text('Kategoriyalar'),
         ],
       ),
     );
